7939086c3673b4fca9fe2ad628611a44758ae191
JuHyeok Kim <juhyeok.dev@gmail.com>
2021-06-09 10:39:33 +0900

    Study components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- {{ message }} -->
+    <!-- <span v-bind:title="message">마우스 올리면 title이 출력됩니다.</span> -->
+    <!-- <p v-if="seen">보인다</p> -->
+    <!-- <ol>
+      <li v-for="todo in todos" :key="todo">{{ todo.text }}</li>
+    </ol> -->
+    <p>{{ message }}</p>
+    <!-- <button v-on:click="reverseMessage">메시지 뒤집기</button> -->
+    <input type="text" v-model="message">
+    <ol>
+      <li v-for="(item, index) in todos" :key="index">
+        <todo :label="item.text" :done="true"></todo>
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from "./components/Todo.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Todo
+  },
+  data() {
+    return {
+      message: "hello!",
+      seen: true,
+      todos: [
+        { text: "Vue.js 공부" },
+        { text: "Spring Security 공부" },
+        { text: "모름" },
+      ]
+    }
+  },
+  methods: {
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('')
+    }
+  },
 }
 </script>
 
